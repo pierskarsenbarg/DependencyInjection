5f543345ef8b5d2531ec918f6bac78ad414d96bf
Piers Karsenbarg <piers.karsenbarg@t101.com>
2014-07-14 21:33:34 +0100

    Added autofac demo

---
+++ DependencyInjection.AutoFac/App_Start/IOCStartup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using DependencyInjection.Common.Classes;
+using DependencyInjection.Common.Interfaces;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(DependencyInjection.AutoFac.App_Start.IOCStartup), "Initialize")]
+
+namespace DependencyInjection.AutoFac.App_Start
+{
+    public static class IOCStartup
+    {
+        public static void Initialize()
+        {
+            #region Create the builder
+            var builder = new ContainerBuilder();
+            #endregion
+
+            #region Setup a common pattern - placed here before RegisterControllers as last one wins
+            builder.RegisterAssemblyTypes().Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerRequest();
+            builder.RegisterAssemblyTypes().Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerRequest();
+            
+            #endregion
+
+            #region Register all controllers for the assembly
+            // Note that ASP.NET MVC requests controllers by their concrete types, so registering them As<IController>() is incorrect. 
+            // Also, if you register controllers manually and choose to specify lifetimes, you must register them as InstancePerDependency() or InstancePerHttpRequest() - 
+            // ASP.NET MVC will throw an exception if you try to reuse a controller instance for multiple requests. 
+            builder.RegisterControllers(typeof(MvcApplication).Assembly).InstancePerRequest();
+
+            #endregion
+
+            #region Register modules
+            builder.RegisterAssemblyModules(typeof(MvcApplication).Assembly);
+            #endregion
+
+            #region Model binder providers - excluded - not sure if need
+            //builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            //builder.RegisterModelBinderProvider();
+            #endregion
+
+            #region Inject HTTP Abstractions
+            /*
+         The MVC Integration includes an Autofac module that will add HTTP request lifetime scoped registrations for the HTTP abstraction classes. The following abstract classes are included: 
+        -- HttpContextBase 
+        -- HttpRequestBase 
+        -- HttpResponseBase 
+        -- HttpServerUtilityBase 
+        -- HttpSessionStateBase 
+        -- HttpApplicationStateBase 
+        -- HttpBrowserCapabilitiesBase 
+        -- HttpCachePolicyBase 
+        -- VirtualPathProvider 
+
+        To use these abstractions add the AutofacWebTypesModule to the container using the standard RegisterModule method. 
+        */
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            builder.RegisterType<Class2>().As<IOutput>();
+            
+
+            #endregion
+
+            #region Set the MVC dependency resolver to use Autofac
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            #endregion
+        }
+    }
+}
---
+++ DependencyInjection.AutoFac/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DependencyInjection.AutoFac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DependencyInjection.AutoFac")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("203cfe65-1899-478e-8ae5-ab0e5a8ca722")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
